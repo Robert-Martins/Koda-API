ebe570f1068f4ecb4bb5f622e07202a2ced53004
Robert-Martins <robertcantares@gmail.com>
2022-06-24 19:38:23 -0300

    feat/implement-enums-attributes-and-methods

--- src/main/java/com/robertmartins/notesapi/enums/UserRolesEnum.java
+++ src/main/java/com/robertmartins/notesapi/enums/UserRolesEnum.java
@@ -1,4 +1,28 @@
 package com.robertmartins.notesapi.enums;
 
 public enum UserRolesEnum {
+
+    ADMIN("Admin", "Administrador"),
+    MANAGER("Manager", "Gerente"),
+    ANALYST("Analyst", "Analista"),
+    REPORTER("Reporter", "Redator"),
+    VIEWER("Viewer", "Observador");
+
+    private final String name;
+
+    private final String description;
+
+    UserRolesEnum(final String name, final String description){
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
 }
