e12c4eae0b846f640e729a8a6af403de03d57eec
Robert-Martins <robertcantares@gmail.com>
2022-06-24 21:14:08 -0300

    feat/implement-enum-dto-and-services

---
+++ src/main/java/com/robertmartins/notesapi/controllers/EnumsController.java
@@ -0,0 +1,47 @@
+package com.robertmartins.notesapi.controllers;
+
+import com.robertmartins.notesapi.dtos.EnumDto;
+import com.robertmartins.notesapi.enums.UfEnum;
+import com.robertmartins.notesapi.enums.UserRolesEnum;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/enums")
+public class EnumsController {
+
+    @GetMapping("/uf")
+    public ResponseEntity<Object> getUfEnums(){
+        return ResponseEntity.status(HttpStatus.OK).body(buildUfEnumList());
+    }
+
+    public List<EnumDto> buildUfEnumList(){
+        List<EnumDto> enumDtoList = new ArrayList();
+        for(UfEnum e : UfEnum.values()){
+            var enumDto = new EnumDto(e.toString(), e.getName());
+            enumDtoList.add(enumDto);
+        }
+        return enumDtoList;
+    }
+
+    @GetMapping("/roles")
+    public ResponseEntity<Object> getRolesEnums(){
+        return ResponseEntity.status(HttpStatus.OK).body(buildRolesEnumList());
+    }
+
+    public List<EnumDto> buildRolesEnumList(){
+        List<EnumDto> enumDtoList = new ArrayList();
+        for(UserRolesEnum e : UserRolesEnum.values()){
+            var enumDto = new EnumDto(e.toString(), e.getDescription());
+            enumDtoList.add(enumDto);
+        }
+        return enumDtoList;
+    }
+
+}
---
+++ src/main/java/com/robertmartins/notesapi/dtos/EnumDto.java
@@ -0,0 +1,16 @@
+package com.robertmartins.notesapi.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class EnumDto {
+
+    private String name;
+
+    private String description;
+
+}
